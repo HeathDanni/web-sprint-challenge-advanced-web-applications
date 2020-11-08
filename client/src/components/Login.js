3ca12af5962e7636ad6df5d530143a6189069dc0
Heather Beckman <heatherbeckman85@gmail.com>
2020-11-08 10:00:15 -0700

    set up PrivateRoute, axiosWithAuths, and form in login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
+  
+  const [form, setForm] = useState({
+    username: "",
+    password: ""
+  });
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  };
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <br></br>
+        <br></br>
+        <label htmlFor='username'>
+          Username:
+          <input
+            name="username"
+            id="username"
+          ></input>
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            name="password"
+            id="password"
+          ></input>
+        </label>
+      </form>
     </>
   );
 };
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+
+    return ( <Route {...rest}
+                render={(props) => {
+                    if (localStorage.getItem('token') === "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98") {
+                        return <Component {...props} />;
+                    } else {
+                        return <Redirect to="/"/>;
+                    }
+                }}
+            />
+        )
+    };
+
+export default PrivateRoute;
